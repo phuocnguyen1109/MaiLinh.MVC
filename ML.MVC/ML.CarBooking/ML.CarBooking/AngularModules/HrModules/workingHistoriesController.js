d1adeb3d290ff0d02ae97293504c77c83239aa35
Hoang Oanh <hoangoanh9899@gmail.com>
2019-07-17 01:18:06 +0700

    added addWorkingHistory modal

---
+++ ML.MVC/ML.CarBooking/ML.CarBooking/AngularModules/HrModules/workingHistoriesController.js
@@ -0,0 +1,31 @@
+﻿(function () {
+    'use strict'
+    angular.module('mainApp').controller('workingHistoriesController', workingHistoriesController);
+
+    function workingHistoriesController($rootScope, $scope, $state) {
+        var vm = this;
+        vm.initialize = initialize;
+
+        function initialize() {
+            getworkingHistories();
+        }
+
+        function getworkingHistories() {
+            //TODO: get data by api
+            var workingHistories = [
+                { id: "1", startDate: "01/01/2016", endDate: "12/12/2018", companyName: "Công Ty TNHH MTV ABC" },
+                { id: "2", startDate: "01/01/2019", endDate: "30/05/2019", companyName: "Công Ty TNHH MTV DEF" },
+            ];
+            vm.workingHistories = workingHistories;
+            console.log(vm.workingHistories);
+        }
+
+
+        $scope.addWorkingHistory = function addWorkingHistory(newWh) {
+            vm.workingHistories.push(newWh);
+            console.log(vm.workingHistories);
+            $scope.newWh = null;
+        }
+    }
+
+})();
