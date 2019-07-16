dbb09365eb856939a5dd1ff30480e2851ac3cb18
nguyenoanh5597@gmail.com <meo19977>
2019-07-17 00:04:55 +0700

    created equipmentController

---
+++ ML.MVC/ML.CarBooking/ML.CarBooking/AngularModules/HrModules/equipmentController.js
@@ -0,0 +1,17 @@
+﻿(function () {
+    'use strict'
+    angular.module('mainApp')
+        .controller('equipmentController', equipmentController);
+
+    function equipmentController($scope, $state) {
+        var vm = this;
+        vm.equipments = [
+            { id: 1, name: "Dù", isProvide: false, date: "" },
+            { id: 2, name: "TÚI XÁCH", isProvide: false, date: "" },
+            { id: 3, name: "BÌA TRÌNH KÝ", isProvide: false, date: "" },
+            { id: 4, name: "SIM ĐIỆN THOẠI", isProvide: false, date: "" },
+        ]
+        console.log("vm", vm)
+
+    }
+})();
