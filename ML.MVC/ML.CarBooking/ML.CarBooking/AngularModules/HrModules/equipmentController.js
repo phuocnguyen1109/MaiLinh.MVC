bb09bafc02d023898b10f1834bce44d178e80173
nguyenoanh5597@gmail.com <meo19977>
2019-07-17 21:08:50 +0700

    added function getEquipments(), getEquipmentsByUserId()

--- ML.MVC/ML.CarBooking/ML.CarBooking/AngularModules/HrModules/equipmentController.js
+++ ML.MVC/ML.CarBooking/ML.CarBooking/AngularModules/HrModules/equipmentController.js
@@ -5,13 +5,54 @@
 
     function equipmentController($scope, $state) {
         var vm = this;
-        vm.equipments = [
-            { id: 1, name: "Dù", isProvide: false, date: "1/1/2019" },
-            { id: 2, name: "TÚI XÁCH", isProvide: false, date: "1/1/2019" },
-            { id: 3, name: "BÌA TRÌNH KÝ", isProvide: false, date: "1/1/2019" },
-            { id: 4, name: "SIM ĐIỆN THOẠI", isProvide: false, date: "1/1/2019" },
-        ]
+        vm.initialize = initialize;
+
+        function initialize() {
+            var userId = "0001";
+            getEquipments();
+            getEquipmentsByUserId(userId);
+
+
+            vm.equipments.forEach(function (item, index) {
+
+                var userEquiments = vm.userEquipments.filter(function (value) {
+                    return value.equipmentId == item.equipmentId;
+                });
+
+                if (userEquiments.length > 0) {
+                    item.receivedDate = new Date(userEquiments[0].receivedDate);
+                    item.isReceive = true;
+                }
+            });
+
+
+        };
+
+
+        function getEquipments() {
+            vm.equipments = [
+                { equipmentId: 1, name: "Dù" },
+                { equipmentId: 2, name: "TÚI XÁCH" },
+                { equipmentId: 3, name: "BÌA TRÌNH KÝ" },
+                { equipmentId: 4, name: "SIM ĐIỆN THOẠI" },
+            ]
+        };
+
+        function getEquipmentsByUserId(userId) {
+            vm.userEquipments = [
+                { id: 1, userId: "0001", equipmentId: 1, receivedDate: "1/1/2019" },
+                { id: 2, userId: "0001", equipmentId: 2, receivedDate: "1/1/2019" },
+                { id: 3, userId: "0001", equipmentId: 4, receivedDate: "3/1/2019" },
+
+            ]
+        };
+
+
         console.log("vm", vm)
 
     }
+
+
+
+
 })();
