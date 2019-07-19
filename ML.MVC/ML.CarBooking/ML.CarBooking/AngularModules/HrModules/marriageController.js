09f6d9a33560f56b016b6cc47a3927b86159457b
nguyenoanh5597@gmail.com <meo19977>
2019-07-19 13:52:09 +0700

    added mariageController

---
+++ ML.MVC/ML.CarBooking/ML.CarBooking/AngularModules/HrModules/marriageController.js
@@ -0,0 +1,38 @@
+﻿(function () {
+    'use strict'
+    angular.module('mainApp')
+        .controller('marriageController', marriageController);
+
+    function marriageController($scope, $state) {
+        var vm = this;
+        vm.initialize = initialize;
+
+        function initialize() {
+            getMarritalStatus();
+            getFamilyMembers()
+        };
+
+
+        function getMarritalStatus() {
+            vm.marritalStatuss = [
+                { marritalStatusId: 1, name: "Độc Thân" },
+                { marritalStatusId: 2, name: "Lập Gia Đình" },
+                { marritalStatusId: 3, name: "Đã Ly Dị" },
+            ]
+        };
+
+        function getFamilyMembers() {
+            vm.familyMembers = [
+                { memberId: 1, memberName: "Nguyễn Văn Phi", dob: 1959, address: "TPHCM", relationship: "Bố", isGone: false },
+                { memberId: 2, memberName: "Nguyễn Thị Nghi", dob: 1959, address: "TPHCM", relationship: "Mẹ", isGone: false },
+            ]
+        };
+
+        console.log("vm", vm)
+
+    }
+
+
+
+
+})();
