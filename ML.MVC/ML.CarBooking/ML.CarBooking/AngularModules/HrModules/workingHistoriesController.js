fd645980502b044d6df8c90f549c996d02d00fe1
Hoang Oanh <hoangoanh9899@gmail.com>
2019-07-21 02:28:27 +0700

    fixed event in workingHistories

--- ML.MVC/ML.CarBooking/ML.CarBooking/AngularModules/HrModules/workingHistoriesController.js
+++ ML.MVC/ML.CarBooking/ML.CarBooking/AngularModules/HrModules/workingHistoriesController.js
@@ -6,12 +6,14 @@
         var vm = this;
         vm.initialize = initialize;
         vm.checkValid = checkValid;
-        vm.add = add;
-
-
+        vm.openAddModal = openAddModal
+        vm.openEditWokingHistory = openEditWokingHistory;
+        vm.openDeleteMModal = openDeleteMModal;
+        vm.saveChanges = saveChanges;
+        vm.deleted = deleted;
 
         vm.isValid = false;
-       
+
 
         function initialize() {
             vm.userWorkingHistory = {
@@ -23,23 +25,40 @@
         function getworkingHistories() {
             //TODO: get data by api
             vm.workingHistories = [
-                { id: "1", startDate: "01/01/2016", endDate: "12/12/2018", companyName: "Công Ty TNHH MTV ABC" },
-                { id: "2", startDate: "01/01/2019", endDate: "05/30/2019", companyName: "Công Ty TNHH MTV DEF" },
+                { id: 1, startDate: "01/01/2016", endDate: "12/12/2018", companyName: "Công Ty TNHH MTV ABC" },
+                { id: 2, startDate: "01/01/2019", endDate: "05/30/2019", companyName: "Công Ty TNHH MTV DEF" },
             ];
             console.log("workingHistories => " + vm.workingHistories);
         }
 
 
-        vm.addWorkingHistory = function addWorkingHistory(newWh) {
-            var newId = new Date();
-            newId = newId.getMilliseconds();
-            newWh.id = newId.toString();
+        function saveChanges() {
+            var r = vm.userWorkingHistory;
+            var s = r.startDate.getMonth() + "/" + r.startDate.getDate() + "/" + r.startDate.getFullYear();
+            var e = r.endDate.getMonth() + "/" + r.endDate.getDate() + "/" + r.endDate.getFullYear();
+            if (r.id) {
+                vm.workingHistories.forEach(function (item, index) {
+                    if (r.id == item.id) {
+                        item.startDate = s;
+                        item.endDate = e;
+                        item.companyName = r.companyName;
+                    }
+                    return;
+                });
+            } else {
+                var newId = new Date();
+                newId = newId.getMilliseconds().toString();
+                vm.userWorkingHistory = {
+                    id: newId, startDate: s, endDate: e, companyName: r.companyName
+                };
 
-            vm.workingHistories.push(newWh);
+                vm.workingHistories.push(vm.userWorkingHistory);
+            };
+
             console.log(vm.workingHistories);
         }
 
-        function add() {
+        function openAddModal() {
             vm.userWorkingHistory = {
                 startDate: null, endDate: null, companyName: null
             };
@@ -63,15 +82,30 @@
             }
         }
 
-        vm.editWokingHistory = function editWokingHistory(workingHistory) {
+        function openEditWokingHistory(r) {
             vm.modalTitle = "Chỉnh sửa lịch sử công tác";
             vm.isValid = true;
-            var sDate = new Date(workingHistory.startDate);
-            var eDate = new Date(workingHistory.endDate);
+            var sDate = new Date(r.startDate);
+            var eDate = new Date(r.endDate);
             console.log(sDate);
             vm.userWorkingHistory = {
-                startDate: sDate, endDate: eDate, companyName: workingHistory.companyName
+                id: r.id, startDate: sDate, endDate: eDate, companyName: r.companyName
             };
+        }
+
+        function openDeleteMModal(row) {
+            vm.deletedDataRow = row;
+            console.log(vm.deletedDataRow);
+        }
+
+        function deleted(r) {
+            var deletedInndex = null;
+            vm.workingHistories.forEach(function (item, index) {
+                if (r.id == item.id) {
+                    deletedInndex = index;
+                }
+            });
+            vm.workingHistories.splice(deletedInndex, 1);
         }
     }
 
