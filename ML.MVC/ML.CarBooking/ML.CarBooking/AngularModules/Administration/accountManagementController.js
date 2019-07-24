dc2613056cac082c76736e66a048b2b1c1525869
nguyenoanh5597@gmail.com <meo19977>
2019-07-24 17:25:53 +0700

    added fake data of Account page

--- ML.MVC/ML.CarBooking/ML.CarBooking/AngularModules/Administration/accountManagementController.js
+++ ML.MVC/ML.CarBooking/ML.CarBooking/AngularModules/Administration/accountManagementController.js
@@ -5,6 +5,20 @@
 
     function accountManagementController($scope, $state) {
         var vm = this;
+        vm.initialize = initialize;
+
+        function initialize() {
+            getAccounts();
+        }
+
+        function getAccounts() {
+            var accounts = [
+                { id: "1", startDate: "01/01/2016", endDate: "12/12/2018", companyName: "Công Ty TNHH MTV ABC" },
+                { id: "2", startDate: "01/01/2019", endDate: "30/05/2019", companyName: "Công Ty TNHH MTV DEF" },
+            ];
+            vm.accounts = accounts;
+            console.log(vm.accounts);
+        }
 
     }
 })();
