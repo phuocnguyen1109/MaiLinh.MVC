09d6c2385dbc00716f19210e13506bf1f175c637
Hoang Oanh <hoangoanh9899@gmail.com>
2019-08-10 01:28:30 +0700

    fixed events

--- ML.MVC/ML.CarBooking/ML.CarBooking/AngularModules/Administration/nationalManagementController.js
+++ ML.MVC/ML.CarBooking/ML.CarBooking/AngularModules/Administration/nationalManagementController.js
@@ -9,34 +9,40 @@
         vm.initialize = initialize;
         vm.rowClick = rowClick;
         vm.checkSelected = checkSelected;
+        vm.checkValid = checkValid;
         vm.filterNational = filterNational;
         vm.add = add;
         vm.addNewNational = addNewNational;
         vm.gotoEdit = gotoEdit;
-        vm.remove = remove;
+        vm.saveChanges = saveChanges;
+        vm.deleted = deleted;
+        vm.deletedRow = deletedRow;
 
         var userId = $state.params.id;
         vm.selectedItem = 1;
         vm.selectedNational = 0;
         vm.listNewNational = [];
+        vm.deleteRows = [];
+
+        vm.isValid = false;
         vm.isBlank = true;
 
         function initialize() {
             //getNational();
             vm.types = [
-                { typeId: 0, name: "Quốc Gia" },
-                { typeId: 1, name: "Thành Phố" },
-                { typeId: 2, name: "Quận, Huyện" }
+                { Type: 0, TypeName: "Quốc Gia" },
+                { Type: 1, TypeName: "Thành Phố" },
+                { Type: 2, TypeName: "Quận, Huyện" }
             ]
 
             vm.nationals = [
-                { nationId: 0, name: 'Việt Nam' },
-                { nationId: 1, name: 'Thái Lan' },
-                { nationId: 2, name: 'Malaysia' }
+                { ParentId: 0, ParentName: 'Việt Nam' },
+                { ParentId: 1, ParentName: 'Thái Lan' },
+                { ParentId: 2, ParentName: 'Malaysia' }
             ];
 
             vm.citys = [
-                { IsSelected: false, Id: 5, Name: "Hồ Chí Minh", ParentId: 0, ParentName: "Việt Nam", Type: 1, TypeName: "Thanh Phố"}
+                { IsSelected: false, Id: 5, Name: "Hồ Chí Minh", ParentId: 0, ParentName: "Việt Nam", Type: 1, TypeName: "Thành Phố" }
             ]
         }
 
@@ -45,7 +51,7 @@
         //    if (!userId) {
         //        return;
         //    }
-            
+
         //}
 
         function filterNational() {
@@ -60,23 +66,33 @@
         }
 
         function checkSelected() {
+            //debugger;
             var selectedIds = angular.copy(vm.citys).filter(function (item) {
                 return item.IsSelected;
             });
 
-            if (selectedIds.length == 1) {
-                selectedId = selectedIds[0].Id;
-                vm.CanEdit = true;
+
+            if (selectedIds.length >= 1) {
+                if (selectedIds.length == 1) {
+                    selectedId = selectedIds[0].Id;
+                    vm.CanEdit = true;
+                } else {
+                    vm.CanEdit = false;
+                }
+                vm.CanDelete = true;
+                vm.deleteRows = selectedIds;
+                console.log(vm.deleteRows);
                 return;
             }
 
             vm.CanEdit = false;
+            vm.CanDelete = false;
         }
 
         function resetModel() {
             vm.nationalModel = {
                 Id: 0,
-                Name: '',
+                Name: "",
                 ParentId: null,
                 Type: null
             };
@@ -88,8 +104,41 @@
             vm.isValid = false;
         }
 
+        function checkValid() {
+            if (vm.nationalModel.Name != "" || !vm.nationalModel.Name) {
+                vm.isValid = true;
+                switch (vm.nationalModel.Type) {
+                    case 0:
+                        vm.isBlank = false;
+                        vm.message = null;
+                        break;
+                    case 1:
+                        if (vm.nationalModel.ParentId != null) {
+                            vm.isBlank = false;
+                            vm.message = null;
+                        }
+                        break;
+                    case 2:
+                        if (!vm.nationalModel.ParentId && !vm.nationalModel.Id) {
+                            vm.isBlank = false;
+                            vm.message = null;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                return;
+            }
+            vm.message = "Không để trống các mục!";
+            vm.isBlank = true;
+        }
+
         function addNewNational(row) {
             console.log(row);
+            row.Id = new Date().getMilliseconds();
+            vm.listNewNational.push(row);
+            resetModel();
+            vm.isBlank = false;
         }
 
         function gotoEdit() {
@@ -104,8 +153,71 @@
             vm.isBlank = false;
         }
 
-        function remove() {
+        function saveChanges() {
+            if (!vm.isValid) {
+                return;
+            }
+            vm.listNewNational.forEach(function (item) {
+                var push = true;
+                vm.citys.forEach(function (i) {
+                    if (i.Id == item.Id) {
+                        i.Name = item.Name;
+                        vm.nationals.forEach(function (n) {
+                            if (n.ParentId == item.ParentId) {
+                                i.ParentId = n.ParentId;
+                                i.ParentName = n.ParentName;
+                                return;
+                            }
+                        });
+                        vm.types.forEach(function (t) {
+                            if (t.Type == item.Type) {
+                                i.Type = t.Type;
+                                i.TypeName = t.TypeName;
+                                return;
+                            }
+                        });
+                        push = false;
+                    } else {
+                        vm.nationals.forEach(function (n) {
+                            if (n.ParentId == item.ParentId) {
+                                item.ParentName = n.ParentName;
+                                return;
+                            }
+                        });
+                        vm.types.forEach(function (t) {
+                            if (t.Type == item.Type) {
+                                item.TypeName = t.TypeName;
+                                return;
+                            }
+                        });
+                    }
+                });
+                if (push) {
+                    vm.citys.push(item);
+                }
+            });
+            vm.listNewNational = [];
+        }
 
+        function deleted() {
+
+            vm.deleteRows.forEach(function (row) {
+                vm.citys.forEach(function (item, index) {
+                    if (row.Id == item.Id) {
+                        vm.citys.splice(index, 1);
+                        return;
+                    }
+                });
+            });
+        }
+
+        function deletedRow(row) {
+            vm.listNewNational.forEach(function (i, index) {
+                if (row.Id == i.Id) {
+                    vm.listNewNational.splice(index, 1);
+                    return;
+                }
+            })
         }
     }
 })();
