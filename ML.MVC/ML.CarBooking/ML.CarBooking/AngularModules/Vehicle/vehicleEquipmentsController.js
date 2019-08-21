e0399f8485aefb83da65e184c0945bb2e50219d4
nguyenoanh5597@gmail.com <meo19977>
2019-08-21 17:49:03 +0700

    added controllers

---
+++ ML.MVC/ML.CarBooking/ML.CarBooking/AngularModules/Vehicle/vehicleEquipmentsController.js
@@ -0,0 +1,40 @@
+﻿(function () {
+    'use strict'
+    angular.module('mainApp').controller('vehicleEquipmentsController', vehicleEquipmentsController);
+
+    function vehicleEquipmentsController($rootScope, $scope, $state) {
+        var vm = this;
+        vm.initialize = initialize;
+
+        function initialize() {
+            getVehicleEquipments();
+            getRequestBys();
+
+        }
+
+        function getVehicleEquipments() {
+            //TODO: get data by api
+            vm.vehicleEquipments = [
+                { id: 1, vehicleName: 'TÚI Y TẾ', recievedDate: Date, expDate:Date, deliveryDateByCar:Date, status: '', requestById: '' },
+                { id: 2, vehicleName: 'DANH SÁCH THUỐC/DỤNG CỤ Y TẾ', recievedDate:Date, expDate:Date, deliveryDateByCar:Date, status: '', rerequestByIdquestOfId: '' },
+                { id: 3, vehicleName: 'ÁO PHẢN QUANG', recievedDate:Date, expDate:Date, deliveryDateByCar:Date, status: '', requestById: '' },
+                { id: 4, vehicleName: 'TAM GIÁC CẢNH BÁO', recievedDate:Date, expDate:Date, deliveryDateByCar:Date, status: '', requestById: '' },
+                { id: 5, vehicleName: 'BÚA THOÁT HIỂM', recievedDate:Date, expDate:Date, deliveryDateByCar:Date, status: '', requestById: '' },
+                { id: 6, vehicleName: 'ĐÈN PIN CHIẾU SÁNG', recievedDate:Date, expDate:Date, deliveryDateByCar:Date, status: '', requestById: '' },
+                { id: 7, vehicleName: 'LƯỚI TRÙM', recievedDate:Date, expDate:Date, deliveryDateByCar:Date, status: '', requestById: '' },
+                { id: 8, vehicleName: 'BÌNH CHỮA CHÁY', recievedDate:Date, expDate:Date, deliveryDateByCar:Date, status: '', requestById: '' }
+            ]
+        }
+
+        function getRequestBys() {
+            vm.requestBys = [
+                { requestById: 1, requestByName: 'NN' },
+                { requestById: 2, requestByName: 'KH' },
+                { requestById: 3, requestByName: 'CTY' }
+            ]
+         
+
+        }
+    }
+
+})();
---
+++ ML.MVC/ML.CarBooking/ML.CarBooking/AngularModules/Vehicle/vehicleFuelManagementController.js
@@ -0,0 +1,23 @@
+﻿(function () {
+    'use strict'
+    angular.module('mainApp').controller('vehicleFuelManagementController', vehicleFuelManagementController);
+
+    function vehicleFuelManagementController($rootScope, $scope, $state) {
+        var vm = this;
+        vm.initialize = initialize;
+
+        function initialize() {
+            getVehicleFuels();
+
+        }
+
+        function getVehicleFuels() {
+            //TODO: get data by api
+            vm.vehicleFuels = [
+                { id: 1, litresQuota: 0.2, fuelTypeQuota: 'RON95', chargedDate: '15/07/2019', starKM: '13092', endKM: '14112', fuelType: 'RON95', litres: 60, gasStation: 'HÓC MÔN 01', diver:'NGUYỄN HỮU PHƯỚC', note:'' },
+            ]
+        }
+
+    }
+
+})();
