04a40b642b5b8212195b1ff7702b7aa99e6e28ab
Hoang Oanh <hoangoanh9899@gmail.com>
2019-08-25 01:43:13 +0700

    added vehicleLoan Controller

---
+++ ML.MVC/ML.CarBooking/ML.CarBooking/AngularModules/Vehicle/vehicleLoanController.js
@@ -0,0 +1,29 @@
+﻿(function () {
+    'use strict'
+    angular.module('mainApp').controller('vehicleLoanController', vehicleLoanController);
+    vehicleLoanController.$inject = ['$http', '$rootScope', '$scope', '$state'];
+
+    function vehicleLoanController($scope, $state, $http, $rootScope) {
+        var vm = this;
+
+        vm.initialize = initialize;
+
+        function initialize() {
+            vm.vehicleLoans = [
+                {
+                    employeeId: 'NV00001', employeeName: 'Nguyễn Hữu Phước', phoneNumber: '0344202279',
+                    receive: { receivedDate: '25/06/2018', receivedTime: '08:30', receivedKM: '1300', receivedFuel: '' },
+                    giveback: { givebackDate: '', givebackTime: '', givebackKM: '', givebackFuel: '' },
+                    note: ''
+                }
+            ]
+        }
+
+        //function getAllVehicleLoans() {
+        //    $http.get('/api/Vehicle/GetAllVehicleLoans')
+        //        .then(function (result) {
+        //            vm.vehicleLoans = result.data;
+        //        });
+        //}
+    }
+})();
