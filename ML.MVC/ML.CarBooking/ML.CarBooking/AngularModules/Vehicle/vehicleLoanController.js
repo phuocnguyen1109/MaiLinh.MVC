e2a270c86f8589214e19887acd3783a40f82fdd0
Hoang Oanh <hoangoanh9899@gmail.com>
2019-08-25 02:03:29 +0700

    added select options for fuel

--- ML.MVC/ML.CarBooking/ML.CarBooking/AngularModules/Vehicle/vehicleLoanController.js
+++ ML.MVC/ML.CarBooking/ML.CarBooking/AngularModules/Vehicle/vehicleLoanController.js
@@ -12,11 +12,18 @@
             vm.vehicleLoans = [
                 {
                     employeeId: 'NV00001', employeeName: 'Nguyễn Hữu Phước', phoneNumber: '0344202279',
-                    receive: { receivedDate: '25/06/2018', receivedTime: '08:30', receivedKM: '1300', receivedFuel: '' },
-                    giveback: { givebackDate: '', givebackTime: '', givebackKM: '', givebackFuel: '' },
+                    receive: { receivedDate: '25/06/2018', receivedTime: '08:30', receivedKM: '1300', receivedFuelId: 1, receivedFuel: '1/4' },
+                    giveback: { givebackDate: '', givebackTime: '', givebackKM: '', givebackFuelId: 2, givebackFuel: '1/2' },
                     note: ''
                 }
-            ]
+            ];
+
+            vm.fuelLevels = [
+                { lvId: 0, lvName: '0' },
+                { lvId: 1, lvName: '1/4' },
+                { lvId: 2, lvName: '1/2' },
+                { lvId: 3, lvName: '1' },
+            ];
         }
 
         //function getAllVehicleLoans() {
