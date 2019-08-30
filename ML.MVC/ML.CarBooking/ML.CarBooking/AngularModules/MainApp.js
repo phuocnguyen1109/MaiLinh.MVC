36b3f64b73eadba88802848b9c8c59dee982b54e
Hoang Oanh <hoangoanh9899@gmail.com>
2019-08-31 02:53:08 +0700

    implement mainPage

--- ML.MVC/ML.CarBooking/ML.CarBooking/AngularModules/MainApp.js
+++ ML.MVC/ML.CarBooking/ML.CarBooking/AngularModules/MainApp.js
@@ -12,32 +12,50 @@
             { id: '0002', picture: 'no_avatar.png', firstName: 'Nguyễn Văn', lastName: 'Hoàng', doB: '01/07/1998', phone: '0344112244', dateIn: '01/07/2018', role: 'Kinh Doanh', typeContract: 'Có Thời Hạn', duration: '1 Năm', expired: '01/07/2019' },
             { id: '0003', picture: 'no_avatar.png', firstName: 'Lê Hoàng Thanh', lastName: 'Trúc', doB: '11/10/1994', phone: '0120778458', dateIn: '01/06/2018', role: 'Thư Ký', typeContract: 'Vô Thời Hạn', duration: '', expired: '' },
         ];
+        vm.isMainPage = true;
+        vm.active = 0;
+        vm.slides = [
+            { id: 1, imgUrl: 'car.png' },
+            { id: 2, imgUrl: '16seats.jpg' },
+            { id: 3, imgUrl: '29seats.png' }
+        ];
 
         vm.gotoEmployees = gotoEmployees;
         vm.gotoVehicleManagement = gotoVehicleManagement;
         vm.gotoMasterDataManagement = gotoMasterDataManagement;
         vm.gotoAccountManagement = gotoAccountManagement;
         vm.gotoNationalManagement = gotoNationalManagement;
+        vm.goMainPage = goMainPage;
 
 
         function gotoEmployees() {
             $state.go('employees');
+            vm.isMainPage = false;
         }
 
         function gotoVehicleManagement() {
             $state.go('vehicle');
+            vm.isMainPage = false;
         }
 
         function gotoMasterDataManagement() {
             $state.go('masterData');
+            vm.isMainPage = false;
         }
 
         function gotoAccountManagement() {
             $state.go('account');
+            vm.isMainPage = false;
         }
 
         function gotoNationalManagement() {
             $state.go('nationalManagement');
+            vm.isMainPage = false;
+        }
+
+        function goMainPage() {
+            $state.go('dashboard');
+            vm.isMainPage = true;
         }
     }
 })();
