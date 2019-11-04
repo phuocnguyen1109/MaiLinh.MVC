122ce3ed88226cb592d6d9ec98e758000aca84b2
Admin <Admin@ADMIN>
2019-11-05 03:34:49 +0700

    update missing scripts

---
+++ ML.MVC/ML.CarBooking/ML.CarBooking/SQL_Scripts/Hr_Update_019.sql
@@ -0,0 +1,12 @@
+﻿ALTER PROC [Hr].[GetPersonHealthCheck]
+@pid INT
+AS
+BEGIN
+
+	SELECT *, 
+	(STUFF((SELECT ',' + CONVERT(VARCHAR(2),c1.HealthStdId)
+FROM [Hr].[PersonHealthStd] C1
+WHERE c1.PersonHealthCheckId = hc.Id
+FOR XML PATH('')),1,1,'')) AS [StandardIds]
+FROM [Hr].[PersonHealthCheck] hc WHERE PersonId =@pid
+END
